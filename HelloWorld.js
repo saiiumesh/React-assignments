b456e2e23dda4e8806e7a78fb7b505c84fbd1f88
umesh <56065659+saiiumesh@users.noreply.github.com>
2022-05-22 10:40:02 +0530

    assignment 1

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>HelloWorld</div>
+    )
+  }
+}
+
+export default HelloWorld
